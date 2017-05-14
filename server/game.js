529555f494419ce708a07372ec5aea880b5cc1cf
Jeremy Taylor <jeremyrt88@gmail.com>
2017-05-14 12:17:40 -0500

    refactor

--- server/game.js
+++ server/game.js
@@ -87,16 +87,16 @@ class Game {
         this.io.to(this.id).emit('updateChatLog', `${player.tag} : ${msg}`)
       })
 
-      /**
-       * Remove player from array of players in this game instance
-       */
-      player.socket.on('leave-game', (id) => {
-        player.socket.leave(this.id)
-        let index = this.players.findIndex((element) => {
-          return element.id === id
-        })
-        this.players.splice(index, 1)
-      })
+      // /**
+      //  * Remove player from array of players in this game instance
+      //  */
+      // player.socket.on('leave-game', (id) => {
+      //   player.socket.leave(this.id)
+      //   let index = this.players.findIndex((element) => {
+      //     return element.id === id
+      //   })
+      //   this.players.splice(index, 1)
+      // })
 
       /**
       * Socket Event to handle requests for a player entering a different room
